c7dcc79216102eb744f2ba0d4e26d8cd360f845c
Ravi Lachhman <42775804+ravilach@users.noreply.github.com>
2022-12-28 12:28:54 -0500

    Update CvExampleApp.java

--- src/main/java/io/harness/cv/example/app/CvExampleApp.java
+++ src/main/java/io/harness/cv/example/app/CvExampleApp.java
@@ -16,9 +16,13 @@ public class CvExampleApp {
 	public static void main(String[] args) {
 		SpringApplication.run(CvExampleApp.class, args);
 		
-		//Start Stable
-		GenerateStableMetrics gsm = new GenerateStableMetrics();
-		gsm.generateAll();
+		//Start Stable for :stable tag
+		//GenerateStableMetrics gsm = new GenerateStableMetrics();
+		//gsm.generateAll();
+
+		//Start Unstable for :unstable tag
+		GenerateUnstableMetrics gusm = new GenerateUnstableMetrics();
+		gusm.generateAll();
 
 	}
 
