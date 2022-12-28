65b2b01e8f63c5a621e1b0d9b811dabea7ff7f02
Ravi Lachhman <42775804+ravilach@users.noreply.github.com>
2022-12-28 11:24:09 -0500

    Update CvExampleApp.java

--- src/main/java/io/harness/cv/example/app/CvExampleApp.java
+++ src/main/java/io/harness/cv/example/app/CvExampleApp.java
@@ -3,11 +3,23 @@ package io.harness.cv.example.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/** 
+ * Spring Boot Start Class for CV Example App.
+ * Boots and generates metrics via the Metric Generator. 
+ * @author Ravi Lachhman
+*/
+
 @SpringBootApplication
 public class CvExampleApp {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CvExampleApp.class, args);
+		
+		//Start Stable
+		GenerateStableMetrics gm = new GenerateStableMetrics();
+		gm.generateAll();
+
 	}
 
 }
